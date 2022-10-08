1254fe239c4d865fc842377a6913d021d23ec1ba
Meh1928 <thejedi64@gmail.com>
2022-10-08 19:36:16 +0800

    char rotation

--- Assets/Scripts/AliceScripts/characterControl.cs
+++ Assets/Scripts/AliceScripts/characterControl.cs
@@ -9,9 +9,10 @@ public class characterControl : MonoBehaviour
     public static bool switchMode; //true = shadowRealm, false = 3d
     private joystickManager joystickManger;
     private Vector3 dir;
-    private float inputX, inputZ;
+    private float inputX, inputY;
     private float moveSpeed;
-
+    Camera cam;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -19,35 +20,39 @@ public class characterControl : MonoBehaviour
         moveSpeed = 15.0f;
         joystickManger = GameObject.Find("joystick_imgBg").GetComponent<joystickManager>();
         Player.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationZ;
+        cam = Camera.main;
     }
 
     public void ToggleSwitch()
     {
         if(playerCandleScript.restrictMode == false)
-        if(switchMode == false)
-        {
-            switchMode = true;
-        }
-        else if (switchMode)
-        {
-            switchMode = false;
-        }
+            switchMode = switchMode switch
+            {
+                false => true,
+                true => false,
+            };
     }
 
     void FixedUpdate()
     {
-
         //player orientation (not fully functional) [testing]
         /*Player.transform.Rotate(Vector3.up * inputX * (100f * Time.deltaTime));*/
-
         /*Player.transform.position += new Vector3(inputX * moveSpeed, 0, inputZ * moveSpeed);*/
-
+        
+        Transform camTransform = cam.transform;
+        
         inputX = joystickManger.inputHorizontal();
-        inputZ = joystickManger.inputVertical();
+        inputY = joystickManger.inputVertical();
+        
+        
+        Vector3 faceVector = (camTransform.forward * inputY + camTransform.right * inputX).normalized;
+        faceVector.y = 0;
+        Vector3 rotation = Vector3.RotateTowards(Player.transform.forward, faceVector,10 * Time.deltaTime,0f);
+        Player.transform.rotation = Quaternion.LookRotation(rotation);
 
         if (switchMode == false)
         {
-            dir = new Vector3(inputX, 0, inputZ).normalized;
+            dir = new Vector3(inputX, 0, inputY).normalized;
             Player.GetComponent<Rigidbody>().AddForce(dir * moveSpeed);
         }
         else if (switchMode)
