5d7a327d275b4343c741de067fe00ff3cbc9960f
AaronTan1 <0129389@kdu-online.com>
2022-10-10 05:08:36 +0800

    Range Indicator Added
    - Material Swapper
    - applied on z-axis puzzle an candlelight (when in range material turns green)

---
+++ Assets/Scripts/PropsScripts/propsIndicator.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class propsIndicator : MonoBehaviour
+{
+    [SerializeField] Material matSwapColor;
+    [SerializeField] Material matOri;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            for (int i = 0; i < gameObject.transform.childCount; i++)
+            {
+                this.transform.GetChild(i).GetComponent<Renderer>().material = matSwapColor;
+            }
+
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        for (int i = 0; i < gameObject.transform.childCount; i++)
+        {
+            this.transform.GetChild(i).GetComponent<Renderer>().material = matOri;
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
