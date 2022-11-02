caf2c2d93cc87fc3c291b6adb107b3fd8e25d74e
AaronTan1 <0129389@kdu-online.com>
2022-11-02 14:09:22 +0800

    Update
    - Added Door Col
    - Expanded Sec B Table Puzzle range indicator

--- Assets/Scripts/AliceScripts/checkpointScript.cs
+++ Assets/Scripts/AliceScripts/checkpointScript.cs
@@ -9,10 +9,13 @@ public class checkpointScript : MonoBehaviour
     public float proceedDelay; //time before entering new Section
     public Animator doorAnim; //tutorial door
 
+    [SerializeField] private Collider doorColDisable;
+
     private void Awake()
     {
         shadowTransitionAnim = false;
         doorAnim.enabled = false;
+        doorColDisable.isTrigger = false;
     }
 
     public void OnTriggerEnter2D(Collider2D collision)
@@ -29,6 +32,7 @@ public class checkpointScript : MonoBehaviour
         yield return new WaitForSeconds(proceedDelay);
         shadowAlice.transform.position = new Vector3(6.0f, 1.7f, 2.5f);
         doorAnim.enabled = true;
+        doorColDisable.isTrigger = true;
         CameraBehaviour.SwapRooms("Room2");
         Invoke(nameof(UnlockTransition), 0.5f);
     }
