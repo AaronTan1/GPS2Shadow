34d1cb0745abf8346bf449431661eb24759c291b
Hage <89595749+HageFX-78@users.noreply.github.com>
2022-11-29 21:21:54 +0800

    Stage1PuzzleBase and some scene update

--- Assets/Scripts/Shadow2DTest/StaticFakeShadow.cs
+++ Assets/Scripts/Shadow2DTest/StaticFakeShadow.cs
@@ -12,9 +12,9 @@ public class StaticFakeShadow : MonoBehaviour
     [SerializeField] Transform lightTR;
 
     [Tooltip("1 by default, 0.5 to offset it by 50%, 2 to 200%")]
-    [Range(0.0f, 2.0f)]
+    [Range(0.0f, 5.0f)]
     [SerializeField] float xSizePercentageOffset = 1;
-    [Range(0.0f, 2.0f)]
+    [Range(0.0f, 5.0f)]
     [SerializeField] float ySizePercentageOffset = 1;
     [Range(-20f, 20f)]
     [SerializeField] float xOffset = 0;
